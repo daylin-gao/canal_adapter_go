3bbcdfa25a97f37834b2dc75a02610191fe919e0
porter <porter@lifesonic.cn>
2020-05-20 14:05:36 +0800

    fix

--- services/test.go
+++ services/test.go
@@ -1,8 +1,6 @@
 package main
 
-import "github.com/gao111/canal-adapter-go/models"
-
 func main() {
-	sync := models.NewSync("test" , "t1")
-	sync.UpdateSync()
+	//sync := models.NewSync("test" , "t1")
+	//sync.UpdateSync()
 }
