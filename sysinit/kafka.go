b4dc5aa45b7ba2709bd9a6aa5f5c5101d81faa11
porter <porter@lifesonic.cn>
2020-06-29 17:53:33 +0800

    文档

--- sysinit/kafka.go
+++ sysinit/kafka.go
@@ -10,6 +10,7 @@ var (
 	KafkaConsumer sarama.Consumer
 )
 
+//init函数每个golang源文件中都可以定义一个init函数。golang系统中，所有的源文件都有自己所属的目录，每一个目录都有对应的包名。在包的引用中，一旦某一个包被使用，则这个包下边的init函数将会被执行，且只执行一次。只执行一次的含义是什么呢？如果一个包被多个地方引用，那么只有在这个包第一次被引用时，才会执行这个包里边的init函数，其他地方对包的再次引用，这个包里边的init函数不会被执行
 func init() {
 	var err error
 	//设置配置
@@ -24,7 +25,7 @@ func init() {
 	//设置使用的kafka版本,如果低于V0_10_0_0版本,消息中的timestrap没有作用.需要消费和生产同时配置
 	kafkaConfig.Version = sarama.V0_11_0_0
 
-	//使用配置,新建一个异步生产者
+	//使用配置,新建一个生产者
 	KafkaProducer, err = sarama.NewSyncProducer([]string{config.Config.Kafka.Host+":"+config.Config.Kafka.Port}, kafkaConfig)
 	if err != nil {
 		panic(err)
